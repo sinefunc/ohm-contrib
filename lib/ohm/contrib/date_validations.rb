11159a904d15ba021c8a84e98c1a17766d586a72
Cyril David <cyx.ucron@gmail.com>
2010-06-29 00:24:31 +0800

    Change DATE to DATE_REGEX for consistency.

--- lib/ohm/contrib/date_validations.rb
+++ lib/ohm/contrib/date_validations.rb
@@ -2,11 +2,11 @@ require 'date'
 
 module Ohm
   module DateValidations
-    DATE = /\A([0-9]{4})-([01]?[0-9])-([0123]?[0-9])\z/
+    DATE_REGEX = /\A([0-9]{4})-([01]?[0-9])-([0123]?[0-9])\z/
     
     def assert_date(att, error = [att, :not_date])
-      if assert_format att, DATE, error
-        m = send(att).to_s.match(DATE)
+      if assert_format att, DATE_REGEX, error
+        m = send(att).to_s.match(DATE_REGEX)
         assert is_date_parseable?(m[1], m[2], m[3]), error
       end
     end
