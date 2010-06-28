045febbf5032438918f4cb5f8df30cc3a85fb8e7
Cyril David <cyx.ucron@gmail.com>
2010-06-29 00:28:04 +0800

    Extract regular expression into DECIMAL_REGEX.

--- lib/ohm/contrib/number_validations.rb
+++ lib/ohm/contrib/number_validations.rb
@@ -1,12 +1,13 @@
 module Ohm
   # This module will include all numeric validation needs.
-  # As of VERSION 0.0.15, Ohm::NumberValidations#assert_decimal
+  # As of VERSION 0.0.27, Ohm::NumberValidations#assert_decimal
   # is the only method provided.
   module NumberValidations
+    DECIMAL_REGEX = /^(\d+)?(\.\d+)?$/
 
   protected
     def assert_decimal(att, error = [att, :not_decimal])
-      assert_format(att, /^(\d+)?(\.\d+)?$/, error)
+      assert_format att, DECIMAL_REGEX, error
     end
   end
 end
