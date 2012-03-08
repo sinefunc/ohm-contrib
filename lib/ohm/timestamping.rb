f74589aed76aa2d1e7b8154c20f152c98446a9f3
Cyril David <cyx.ucron@gmail.com>
2012-03-09 00:39:18 +0800

    Re-implement timestamping without callbacks (via @jano).

--- lib/ohm/timestamping.rb
+++ lib/ohm/timestamping.rb
@@ -11,31 +11,30 @@ module Ohm
   #   end
   #
   #   post = Post.create
-  #   post.created_at.to_s == Time.now.utc.to_s
+  #   post.created_at.to_i == Time.now.utc.to_i
   #   # => true
   #
   #   post = Post[post.id]
   #   post.save
-  #   post.updated_at.to_s == Time.now.utc.to_s
+  #   post.updated_at.to_i == Time.now.utc.to_i
   #   # => true
   module Timestamping
     def self.included(model)
-      model.send :include, Ohm::Callbacks
-
       model.attribute :created_at, DataTypes::Type::Timestamp
       model.attribute :updated_at, DataTypes::Type::Timestamp
-
-      model.before :create, :initialize_created_at
-      model.before :save,   :initialize_updated_at
     end
 
-  protected
-    def initialize_created_at
-      self.created_at ||= Time.now.utc.to_i
-    end
+    def save!
+      new = new?
 
-    def initialize_updated_at
-      self.updated_at = Time.now.utc.to_i
+      super do |t|
+        t.before do
+          self.created_at = Time.now.utc.to_i if new
+          self.updated_at = Time.now.utc.to_i
+        end
+
+        yield t if block_given?
+      end
     end
   end
 end
