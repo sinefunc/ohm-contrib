0cdd22fff6bd25d3774ff6a49575541911acb2fc
Francesco Rodriguez <frodsan@me.com>
2013-12-24 19:49:28 -0500

    Update Ohm::Timestamps tests.

--- test/helper.rb
+++ test/helper.rb
@@ -1,14 +1,8 @@
 require "cutest"
 require "ohm"
-require "override"
-
-NOW = Time.utc(2010, 5, 12)
-
-include Override
 
 prepare do
   Ohm.flush
-  override(Time, now: NOW)
 end
 
 def assert_nothing_raised(*exceptions)
--- test/timestamp.rb
+++ test/timestamp.rb
@@ -1,31 +1,40 @@
-# encoding: UTF-8
-
-require File.expand_path("./helper", File.dirname(__FILE__))
+require_relative "helper"
+require "override"
+require_relative "../lib/ohm/timestamps"
 
 class Person < Ohm::Model
   include Ohm::Timestamps
 end
 
+NOW = Time.utc(2010, 5, 12)
+
+include Override
+
+prepare do
+  override(Time, now: NOW)
+end
+
 test "a new? record" do
-  assert nil == Person.new.created_at
-  assert nil == Person.new.updated_at
+  assert_equal nil, Person.new.created_at
+  assert_equal nil, Person.new.updated_at
 end
 
 test "on create" do
   person = Person.create
   person = Person[person.id]
 
-  assert NOW == person.created_at
-  assert NOW == person.updated_at
+  assert_equal NOW, person.created_at
+  assert_equal NOW, person.updated_at
 end
 
 test "on update" do
   person = Person.create
 
-  override(Time, :now => Time.utc(2010, 5, 13))
+  override(Time, now: Time.utc(2010, 5, 13))
   person.save
+
   person = Person[person.id]
 
-  assert NOW == person.created_at
-  assert Time.utc(2010, 5, 13) == person.updated_at
+  assert_equal NOW, person.created_at
+  assert_equal Time.utc(2010, 5, 13), person.updated_at
 end
